bd2ef9fadca268e06ab7b8f3d170931f654a8a49
Jose Agustin Velasco Torres <agusmac@MacBook-Pro-de-Jose.local>
2021-02-19 16:50:22 -0600

    CheckPointOne start

---
+++ src/main/java/com/aguslb/checkpointOne/CheckpointOneApplication.java
@@ -0,0 +1,13 @@
+package com.aguslb.checkpointOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckpointOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckpointOneApplication.class, args);
+	}
+
+}
